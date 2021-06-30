5b4610f9bf03e861e1d71ba9ae6291d785218eb9
GusDeps <65987352+G-Deps@users.noreply.github.com>
2021-06-30 17:34:10 +0000

    Código comentado

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-struct tComplexo{
+struct tComplexo{ //struct do exercicio avaliativo sem as operações
   int real;
   int imaginario;
   tComplexo* proximo;
 };
-tComplexo* criaComplexo(int NumReal, int NumImaginario){
+tComplexo* criaComplexo(int NumReal, int NumImaginario){ // analogo ao criaNo para criação de nó "complexo"
   tComplexo* complexo = new tComplexo;
 
   complexo -> real = NumReal;
@@ -17,28 +17,28 @@ tComplexo* criaComplexo(int NumReal, int NumImaginario){
 
   return complexo;
 }
-struct tLista{
+struct tLista{ //struct de gerenciamento da lista encadeada
   tComplexo* primeiro;
   tComplexo* ultimo;
   tComplexo* marcador;
   int tamanho;
 };
-void iniciaLista(tLista* pLista){
+void iniciaLista(tLista* pLista){ //no caso de uma lista vazia essa função popula a primeira vez
   pLista -> primeiro = NULL;
   pLista -> ultimo = NULL;
   pLista -> marcador = NULL;
   pLista -> tamanho = 0;
 }
-int obterTamanho(tLista* pLista){
+int obterTamanho(tLista* pLista){ //funcao para retornar tamanho da lista
   return pLista->tamanho;
 }
-bool listaVazia(tLista* pLista){
+bool listaVazia(tLista* pLista){ //funçaõ para checar se a lista esta vazia
   return (pLista -> tamanho == 0);
 }
-bool finalLista(tLista* pLista){
+bool finalLista(tLista* pLista){ //função para checar se aquele nó é o último da lista
   return (pLista -> marcador == NULL);
 }
-void incluirNoFim(tLista* pLista, int real, int imaginario){
+void incluirNoFim(tLista* pLista, int real, int imaginario){ //função para incluir nó ao final da lista encadeada
   tComplexo* complexo = criaComplexo(real, imaginario);
 
   if(listaVazia(pLista)){
@@ -51,7 +51,7 @@ void incluirNoFim(tLista* pLista, int real, int imaginario){
   pLista -> marcador = complexo;
   pLista -> tamanho++;
 }
-void gravarLista(tLista* pLista,ofstream* outfile){
+void gravarLista(tLista* pLista,ofstream* outfile){ //função para gravar valores da lista encadeada em um arquivo .txt
   pLista -> marcador = pLista-> primeiro;
 
   while(!finalLista(pLista)){
@@ -66,7 +66,7 @@ void gravarLista(tLista* pLista,ofstream* outfile){
   *outfile << flush;
   
 }
-void escrever(int quant,tLista* pLista){
+void escrever(int quant,tLista* pLista){ // função para receber os valores de numeros complexos e organizá-los em uma lista encadeada para depois gravar no arquivo .txt
   ofstream arquivo("db.txt", ios::out );
   if (! arquivo)
   { cout << "Arquivo db.txt nao pode ser aberto" << endl;
@@ -85,7 +85,7 @@ void escrever(int quant,tLista* pLista){
   arquivo.close();
   free(pLista);
 }
-void ler(){
+void ler(){ //Função para abrir, extrair dados de arquivo .txt e orgqanizá-los em uma lista encadeada para depois mostrar os valores direto da lista encadeada
   int real,imaginario,escreve=0;
   string valor;
   tLista* leitura = new tLista;
@@ -138,8 +138,8 @@ void ler(){
 
 int main() {
   int loop = 1;
-  tLista* Complexo = new tLista;
-  iniciaLista(Complexo);
+  tLista* Complexo = new tLista; // cria a lista que iremos utilizar
+  iniciaLista(Complexo); //como acabou de ser criada, inicializa-se ela
   while(loop ==1){
     int input;
     cout<<"Digite 1 para escrever / 2 para ler / 3 para sair\n";
@@ -155,7 +155,6 @@ int main() {
           }else{
             cout<<"Quantidade inválida\n";
           }
-          
         break;
       case (2):
           ler();
